feb77f639379a6dff5cb5c867e89a0b15fa744cc
irconde <irconde@ualr.edu>
2020-07-25 16:37:02 -0500

    View binding enabled

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,47 +2,40 @@ package com.bignerdranch.android.geoquiz;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bignerdranch.android.geoquiz.databinding.ActivityQuizBinding;
 import com.bignerdranch.android.geoquiz.model.Quiz;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private Button mTrueButton;
-    private Button mFalseButton;
-    private Button mNextButton;
-    private TextView mQuestionTextView;
-    private TextView mResultTextView;
+    private ActivityQuizBinding binding;
     private Quiz quiz;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
-        mQuestionTextView = findViewById(R.id.question_text_view);
-        mResultTextView = findViewById(R.id.result_text_view);
 
-        mTrueButton = findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityQuizBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkAnswer(true);
             }
         });
 
-        mFalseButton = findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
+        binding.falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkAnswer(false);
             }
         });
 
-        mNextButton = findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
+        binding.nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToNextQuestion();
@@ -73,26 +66,26 @@ public class QuizActivity extends AppCompatActivity {
             answerColor = getResources().getColor(R.color.red);
         }
 
-        mResultTextView.setText(messageResId);
-        mResultTextView.setTextColor(answerColor);
+        binding.resultTextView.setText(messageResId);
+        binding.resultTextView.setTextColor(answerColor);
         onQuestionAnswered();
     }
 
     private void updateQuestionView() {
-        mQuestionTextView.setText(quiz.getCurrentQuestion().getTextResId());
+        binding.questionTextView.setText(quiz.getCurrentQuestion().getTextResId());
     }
 
     private void reset() {
-        mResultTextView.setText("");
-        mTrueButton.setEnabled(true);
-        mFalseButton.setEnabled(true);
-        mNextButton.setEnabled(false);
+        binding.resultTextView.setText("");
+        binding.trueButton.setEnabled(true);
+        binding.falseButton.setEnabled(true);
+        binding.nextButton.setEnabled(false);
     }
 
     private void onQuestionAnswered() {
-        mTrueButton.setEnabled(false);
-        mFalseButton.setEnabled(false);
-        mNextButton.setEnabled(true);
+        binding.trueButton.setEnabled(false);
+        binding.falseButton.setEnabled(false);
+        binding.nextButton.setEnabled(true);
     }
 
 }
